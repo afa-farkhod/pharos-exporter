00e06b6012945718a0fbbaa2dd091ec924a888c5
reindeer002 <lsj000424@gmail.com>
2025-12-30 09:07:25 +0000

    fix: change default rpc url for right thing

--- cmd/start.go
+++ cmd/start.go
@@ -18,7 +18,7 @@ func runStart(args []string) error {
 	fs := flag.NewFlagSet("start", flag.ContinueOnError)
 	fs.SetOutput(os.Stdout)
 
-	rpcURL := fs.String("rpc", "https://atlantic.dplabs-internal.com/", "JSON-RPC endpoint")
+	rpcURL := fs.String("rpc", "https://atlantic-rpc.dplabs-internal.com/", "JSON-RPC endpoint")
 	myBlsKey := fs.String("my-bls-key", "", "my BLS pubkey (0x...)")
 	checkBlockProof := fs.Bool("check-block-proof", true, "check signedBlsKeys trigger")
 	checkValidatorSet := fs.Bool("check-validator-set", true, "check validator set trigger")
